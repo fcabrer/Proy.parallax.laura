ec4a60001ba63eda0de3baa1cde025900c690aef
fcabrer <fcabrera.dnrpa@gmail.com>
2023-03-31 13:31:53 -0400

    first commit

---
+++ scrip.js
@@ -0,0 +1,6 @@
+let scene = document.getElementById ("scene");
+let text = document.getElementById ("Text");
+
+var parallaxInstance = new Parallax (scene);
+var parallaxInstance = new Parallax (text);
+
